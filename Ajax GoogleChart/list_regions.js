28fda0df66b41ec226d9696ad9cbac49e14d0234
aquispearr <aquispearr@unsa.edu.com>
2024-05-18 16:20:52 -0500

    Html y javascript para calcular y mostrar confirmados

--- Ajax GoogleChart/list_regions.js
+++ Ajax GoogleChart/list_regions.js
@@ -2,9 +2,8 @@ $(document).ready(function() {
     $('#btnMostrarRegiones').click(function() {
         $.getJSON('data.json', function(data) {
             const regionList = $('#regionList');
-            regionList.empty(); // Limpiar la lista antes de agregar nuevas regiones
+            regionList.empty();
 
-            // Iterar sobre los datos y agregar cada región a la lista
             data.forEach(regionData => {
                 const li = $('<li>').text(regionData.region);
                 regionList.append(li);
